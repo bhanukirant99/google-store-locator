73d1e05b326269c07eff5677495ebb5be2c62bf4
bhanukirant99 <60040226+bhanukirant99@users.noreply.github.com>
2020-04-29 01:24:08 +0530

    Add files via upload

---
+++ Google-Store-Locator/google-maps-challenge-master/js/index.js
@@ -0,0 +1,236 @@
+
+window.onload = function() {
+}
+
+var map;
+var markers = [];
+var infoWindow;
+
+function initMap() {
+    var losAngeles = {
+        lat: 34.063380, 
+        lng: -118.358080
+    };
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: losAngeles,
+        zoom: 11,
+        mapTypeId: 'roadmap',
+        styles: [
+            {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+            {
+              featureType: 'administrative.locality',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'poi',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'geometry',
+              stylers: [{color: '#263c3f'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#6b9a76'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry',
+              stylers: [{color: '#38414e'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#212a37'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#9ca5b3'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry',
+              stylers: [{color: '#746855'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#1f2835'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#f3d19c'}]
+            },
+            {
+              featureType: 'transit',
+              elementType: 'geometry',
+              stylers: [{color: '#2f3948'}]
+            },
+            {
+              featureType: 'transit.station',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'geometry',
+              stylers: [{color: '#17263c'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#515c6d'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.stroke',
+              stylers: [{color: '#17263c'}]
+            }
+          ]
+
+    });
+    infoWindow = new google.maps.InfoWindow();
+    searchStores();
+}
+
+function searchStores(){
+    var foundStores = [];
+    var zipCode = document.getElementById('zip-code-input').value;
+    if(zipCode){
+        for(var store of stores){
+            var postal = store['address']['postalCode'].substring(0, 5);
+            if(postal == zipCode){
+                foundStores.push(store);
+            }
+        }
+    } else {
+        foundStores = stores;
+    }
+    clearLocations();
+    displayStores(foundStores);
+    showStoresMarkers(foundStores);
+    setOnClickListener();
+}
+
+function clearLocations(){
+    infoWindow.close();
+    for (var i = 0; i < markers.length; i++) {
+      markers[i].setMap(null);
+    }
+    markers.length = 0;
+}
+
+function setOnClickListener(){
+    var storeElements = document.querySelectorAll('.store-container');
+    storeElements.forEach(function(elem, index){
+        elem.addEventListener('click', function(){
+            new google.maps.event.trigger(markers[index], 'click');
+        })
+    })
+}
+
+function displayStores(stores){
+    var storesHtml = '';
+    for(var [index, store] of stores.entries()){
+        var address = store['addressLines'];
+        var phone = store['phoneNumber'];
+        storesHtml += `
+            <div class="store-container">
+                <div class="store-container-background">
+                    <div class="store-info-container">
+                        <div class="store-address">
+                            <span>${address[0]}</span>
+                            <span>${address[1]}</span>
+                        </div>
+                        <div class="store-phone-number">${phone}</div>
+                    </div>
+                    <div class="store-number-container">
+                        <div class="store-number">
+                            ${index+1}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `
+        document.querySelector('.stores-list').innerHTML = storesHtml;
+    }
+}
+
+
+function showStoresMarkers(stores){
+    var bounds = new google.maps.LatLngBounds();
+    for(var [index, store] of stores.entries()){
+        var latlng = new google.maps.LatLng(
+            store["coordinates"]["latitude"],
+            store["coordinates"]["longitude"]);
+        var name = store["name"];
+        var address = store["addressLines"][0];
+        var openStatusText = store["openStatusText"];
+        var phoneNumber = store["phoneNumber"];
+        var countrySubCode = store["address"]["countrySubdivisionCode"];
+        var countryCode = store["address"]["countryCode"];
+        var pinCode = store["address"]["postalCode"];
+        bounds.extend(latlng);
+        console.log(countrySubCode);
+        createMarker(latlng, name, address, openStatusText, phoneNumber, countrySubCode, countryCode, pinCode, index+1);
+    }
+    map.fitBounds(bounds);
+}
+
+function createMarker(latlng, name, address, openStatusText, phoneNumber, countrySubCode, countryCode, pinCode, index){
+    var html = `
+        <div class="store-info-window">
+            <div class="store-info-image">
+                <img class="image" src="https://inhabitat.com/wp-content/blogs.dir/1/files/2014/07/First-Starbucks-Colombia-Store-2.jpg">
+            </div>
+            <div class="store-info-name">
+                ${name}
+            </div>
+            <div class="store-info-status">
+                ${openStatusText}
+            </div>
+            <div class="store-info-address">
+                <div class="circle">
+                    <i class="fas fa-location-arrow"></i>
+                </div>
+                <div class="store-info-link">
+                  <a class="store-link" href="https://maps.google.com/?q=${name} ${address} ${countrySubCode} ${countryCode} ${pinCode} " target="_blank">${address}</a>
+                </div>    
+            </div>
+            <div class="store-info-phone">
+                <div class="circle">
+                    <i class="fas fa-phone-alt"></i>
+                </div>
+                ${phoneNumber}
+            </div>
+        </div>
+    `;
+    
+      var image = 'https://img.icons8.com/ios-glyphs/2x/coffee.png';
+    
+    var marker = new google.maps.Marker({
+      map: map,
+      position: latlng,
+      animation: google.maps.Animation.DROP,
+      label: index.toString(),
+      icon: image
+    });
+    google.maps.event.addListener(marker, 'click', function() {
+        if (marker.getAnimation() !== null) {
+            marker.setAnimation(null);
+          } else {
+            marker.setAnimation(google.maps.Animation.BOUNCE);
+          }
+      infoWindow.setContent(html);
+      infoWindow.open(map, marker);
+    });
+    markers.push(marker);
+}
